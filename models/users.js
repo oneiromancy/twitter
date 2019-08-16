77f23d1156ccc46711473699e8a1f1c0b5dca60d
oneiromancy <commaintel@gmail.com>
2019-08-16 17:24:57 +0100

    adding user schema to models folder

---
+++ models/users.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// authenticate input against database documents
+UserSchema.statics.authenticate = (email, password, callback) => {
+    User.findOne({ email: email }).exec((error, user) => {
+        if (error) {
+            return callback(error);
+        } else if (!user) {
+            let err = new Error("User not found.");
+            err.status = 401;
+            return callback(err);
+        }
+        bcrypt.compare(password, user.password, (error, result) => {
+            if (result === true) {
+                return callback(null, user);
+            } else {
+                return callback();
+            }
+        });
+    });
+};
+
+// hash password before saving to database
+UserSchema.pre("save", function(next) {
+    const user = this;
+    bcrypt.hash(user.password, 10, function(err, hash) {
+        if (err) return next(err);
+
+        user.password = hash;
+        next();
+    });
+});
+
+const Users = mongoose.model("Users", UserSchema);
+module.exports = Users;
