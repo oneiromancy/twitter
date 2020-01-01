84728b88e322fc183b5d28fe7dde4aa33259196e
oneiromancy <commaintel@gmail.com>
2020-01-01 23:15:41 +0000

    adding a route for user search

---
+++ routes/search.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const users = require("../controllers/users");
+
+router.get("/", users.getUsersByQuery);
+
+module.exports = router;
