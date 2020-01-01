de4b4c316e52cb7e388fa0d9bd5d8b77b03d9da1
oneiromancy <commaintel@gmail.com>
2020-01-01 23:09:45 +0000

    adding follow/unfollow functionality

--- controllers/users.js
+++ controllers/users.js
@@ -8,3 +8,42 @@ exports.getUserIdFromUsername = (req, res, next) => {
         next();
     });
 };
+
+exports.followUser = (req, res, next) => {
+    User.findOneAndUpdate(
+        { username: req.params.username },
+        { $push: { followers: req.session.user._id } }
+    ).exec((err, profile) => {
+        if (err) next(err);
+
+        User.updateOne(
+            { _id: req.session.user._id },
+            { $push: { following: profile._id } }
+        ).exec((err, result) => {
+            if (err) next(err);
+
+            req.session.user.following.push(profile._id);
+            return res.redirect("/");
+        });
+    });
+};
+
+exports.unfollowUser = (req, res, next) => {
+    User.findOneAndUpdate(
+        { username: req.params.username },
+        { $pull: { followers: req.session.user._id } }
+    ).exec((err, profile) => {
+        if (err) next(err);
+
+        User.updateOne(
+            { _id: req.session.user._id },
+            { $pull: { following: profile._id } }
+        ).exec((err, result) => {
+            if (err) next(err);
+
+            const index = req.session.user.following.indexOf(profile._id);
+            req.session.user.following.splice(index, 1);
+            return res.redirect("/");
+        });
+    });
+};
