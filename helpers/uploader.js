3a3093f3619e0ef708547cdc5e1639cbfd33d6f4
oneiromancy <commaintel@gmail.com>
2020-01-01 22:25:41 +0000

    storing user-uploaded image files into server (filtering type of image and limiting file size)

---
+++ helpers/uploader.js
@@ -0,0 +1,33 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(
+            null,
+            new Date().toISOString().replace(/:/g, "-") + file.originalname
+        );
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === "image/jpeg" ||
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/gif"
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+exports.upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
