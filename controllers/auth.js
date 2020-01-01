ddfb824eca9c038a6f05c20ed1fd1d2b19fdc004
oneiromancy <commaintel@gmail.com>
2020-01-01 22:04:39 +0000

    creating user registration functionality and serving sign up page

---
+++ controllers/auth.js
@@ -0,0 +1,39 @@
+const User = require("../models/users");
+const { validationResult } = require("express-validator");
+
+exports.signupUser = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        req.session.errors = errors.array();
+        return res.redirect("/signup");
+    } else {
+        const { fullname, email, username, password } = req.body;
+
+        const data = {
+            fullname,
+            email,
+            username,
+            password
+        };
+
+        User.create(data, (err, user) => {
+            if (err) next(err);
+
+            req.session.user = user;
+            req.session.errors = null;
+
+            return res.redirect("/");
+        });
+    }
+};
+
+exports.renderSignupPage = (req, res, next) => {
+    const errors = req.session.errors;
+    req.session.errors = false;
+
+    return res.render("pages/signup", {
+        title: "Sign up for Twitter",
+        errors
+    });
+};
