30f49fdc2517d9ec79913fc138b85bdcfedc4f4d
oneiromancy <commaintel@gmail.com>
2020-01-01 23:08:55 +0000

    obtains user id from username

--- controllers/users.js
+++ controllers/users.js
@@ -1,63 +1,10 @@
-const Users = require("../models/users");
+const User = require("../models/users");
 
-exports.registerUser = (req, res, next) => {
-    const { fullname, email, username, password } = req.body;
+exports.getUserIdFromUsername = (req, res, next) => {
+    User.findOne({ username: req.params.username }).exec((err, user) => {
+        if (err) next(err);
 
-    if (fullname && email && username && password) {
-        // create object with form input
-        var userData = {
-            fullname,
-            email,
-            username,
-            password
-        };
-
-        // use schema's `create` method to insert document into Mongo
-        Users.create(userData, function(error, user) {
-            if (error) {
-                return next(error);
-            } else {
-                return res.redirect("/");
-            }
-        });
-    } else {
-        const err = new Error("All fields required.");
-        err.status = 400;
-        return next(err);
-    }
-};
-
-exports.loginUser = (req, res, next) => {
-    const { username, password } = req.body;
-
-    if (username && password) {
-        Users.authenticate(username, password, function(error, user) {
-            if (error || !user) {
-                const err = new Error("Wrong password.");
-                err.status = 401;
-                return next(err);
-            } else {
-                req.session.userId = user._id;
-                return res.redirect("/");
-            }
-        });
-    } else {
-        var err = new Error("Username and password are required.");
-        err.status = 401;
-        return next(err);
-    }
-};
-
-exports.logoutUser = (req, res, next) => {
-    console.log("testing");
-    if (req.session) {
-        // delete session object
-        req.session.destroy(function(err) {
-            if (err) {
-                return next(err);
-            } else {
-                return res.redirect("/login");
-            }
-        });
-    }
+        req.profileRequest = user;
+        next();
+    });
 };
