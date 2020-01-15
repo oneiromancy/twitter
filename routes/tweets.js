9d6272e1009fc3abf84195a014935c6a4c9837cc
oneiromancy <commaintel@gmail.com>
2020-01-15 18:22:14 +0000

    adding routes that enable me to do CRUD operations on tweet comments

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const tweets = require("../controllers/tweets");
+const comments = require("../controllers/comments");
 const { upload } = require("../helpers/uploader");
 
+// Tweets
 router.post("/", upload.single("image"), tweets.createTweet);
-
 router.patch("/:tweetId", upload.single("image"), tweets.updateTweet);
-
-router.patch("/:tweetId/like", tweets.likeTweet);
-router.patch("/:tweetId/unlike", tweets.unlikeTweet);
-
 router.delete("/:tweetId", tweets.deleteTweet);
+
+// Tweet Comments
+router.post(
+    "/:tweetId/comments",
+    upload.single("image"),
+    comments.createComment
+);
+router.patch(
+    "/:tweetId/comments/:commentId",
+    upload.single("image"),
+    comments.updateComment
+);
+router.delete("/:tweetId/comments/:commentId", comments.deleteComment);
+
+// Tweet Likes
+router.post("/:tweetId/likes", tweets.likeTweet);
+router.delete("/:tweetId/likes", tweets.unlikeTweet);
 
 module.exports = router;
